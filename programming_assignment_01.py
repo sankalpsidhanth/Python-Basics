4473587ffb8e76be78b7c2ad863e21ef87d34b29
sankalpsidhanth <86710693+sankalpsidhanth@users.noreply.github.com>
2022-05-26 11:47:12 +0530

    Add files via upload

---
+++ programming_assignment_01.py
@@ -0,0 +1,41 @@
+#Q1
+print("Hello World")
+
+print("_______________________")
+
+#Q2
+a=float(input("Enter first number: "))
+b=float(input("Enter second number: "))
+print("Addition: ",a+b)
+print("Division",a/b)
+
+print("_______________________")
+
+
+#Q3
+base=float(input("Enter base of the triangle in centimeters: "))
+height=float(input("Enter height of the triangle in centimeters: "))
+
+Area= 0.5*(base*height)
+print("Area of the triangle is ",Area,"cms square")
+
+
+print("_______________________")
+
+#Q4
+a=10
+b=20
+temp=a
+a=b
+b=temp
+temp=b
+print(a)
+print(b)
+
+print("_______________________")
+
+#Q5
+import random
+#To print any random number
+a=random.random()
+print(a)
